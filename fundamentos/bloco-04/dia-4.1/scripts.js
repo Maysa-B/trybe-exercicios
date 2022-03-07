d199e14c432f7462093e8dfd4e9575ec6e089613
Maysa <maybsalvalaio@gmail.com>
2022-03-07 17:12:24 -0300

    exercícios 4.1

---
+++ fundamentos/bloco-04/dia-4.1/scripts.js
@@ -0,0 +1,17 @@
+// atividade 1:
+const myName = 'Maysa';
+
+const birthCity = 'Meleiro';
+
+let birthYear = 2003;
+// console.log (myName);
+// console.log (birthCity);
+// console.log (birthYear);
+
+birthYear = 2030;
+// console.log (birthYear);
+
+birthCity = 'Paris';
+// console.log (birthCity);
+
+// atividade 2:
