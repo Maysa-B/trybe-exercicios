0f23e8fcd7047f1985638e1c14949101044dadd0
Maysa <maybsalvalaio@gmail.com>
2022-03-14 18:27:29 -0300

    Commit Inicial - dia 5.1

---
+++ fundamentos/bloco-05/dia-5.1/testes/seletores3/seletores3.js
@@ -0,0 +1,17 @@
+const header = document.getElementById('header-container');
+
+header.style.backgroundColor = 'lightgreen';
+
+document.getElementsByClassName('emergency-tasks') [0].style.backgroundColor = 'purple';
+
+for (let i = 0; i <= document.querySelectorAll('.emergency-tasks div h3').length; i += 1) {
+    document.querySelectorAll('.emergency-tasks div h3')[i].style.backgroundColor = 'blue'
+}
+
+document.getElementsByClassName('no-emergency-tasks') [0].style.backgroundColor = 'yellow';
+
+for (let i = 0; i <= document.querySelectorAll('.no-emergency-tasks div h3').length; i += 1) {
+      document.querySelectorAll('.no-emergency-tasks div h3')[i].style.backgroundColor = 'red'
+}
+
+document.getElementById('footer-container').style.backgroundColor = 'green';
