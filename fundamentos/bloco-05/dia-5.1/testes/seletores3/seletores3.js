a326d66904ec5d78334bb9fcdb2b1401f5a3ccd4
Maysa <maybsalvalaio@gmail.com>
2022-03-14 19:16:20 -0300

    Exercícios - 5.1

--- fundamentos/bloco-05/dia-5.1/testes/seletores3/seletores3.js
+++ fundamentos/bloco-05/dia-5.1/testes/seletores3/seletores3.js
@@ -4,13 +4,13 @@ header.style.backgroundColor = 'lightgreen';
 
 document.getElementsByClassName('emergency-tasks') [0].style.backgroundColor = 'purple';
 
-for (let i = 0; i <= document.querySelectorAll('.emergency-tasks div h3').length; i += 1) {
+for (let i = 0; i < document.querySelectorAll('.emergency-tasks div h3').length; i += 1) {
     document.querySelectorAll('.emergency-tasks div h3')[i].style.backgroundColor = 'blue'
 }
 
 document.getElementsByClassName('no-emergency-tasks') [0].style.backgroundColor = 'yellow';
 
-for (let i = 0; i <= document.querySelectorAll('.no-emergency-tasks div h3').length; i += 1) {
+for (let i = 0; i < document.querySelectorAll('.no-emergency-tasks div h3').length; i += 1) {
       document.querySelectorAll('.no-emergency-tasks div h3')[i].style.backgroundColor = 'red'
 }
 
