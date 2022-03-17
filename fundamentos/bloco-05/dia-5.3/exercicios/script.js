c8015d5ab46904020c262e182e9ffc716218185b
Maysa <maybsalvalaio@gmail.com>
2022-03-17 19:15:07 -0300

    Exercícios - 5.3

--- fundamentos/bloco-05/dia-5.3/exercicios/script.js
+++ fundamentos/bloco-05/dia-5.3/exercicios/script.js
@@ -23,7 +23,6 @@ function createDays() {
     for (let index = 0; index < dezDaysList.length; index += 1) {
         const criando = document.createElement('li');
         let dia = dezDaysList[index];
-        console.log (dia)
         criando.innerHTML = dia;
 
         if (dia == 24 || dia == 31) {
@@ -44,7 +43,7 @@ function createDays() {
 
 createDays();
 
-function createButtonHoliday (feriados) {
+function createButtonHoliday(feriados) {
     let container = document.getElementsByClassName('buttons-container')[0];
     let newButton = document.createElement('button');
     newButton.innerText = feriados;
@@ -54,3 +53,151 @@ function createButtonHoliday (feriados) {
 }
 
 createButtonHoliday('Feriados');
+
+function changeColorHoliday() {
+    let click = document.getElementById('btn-holiday');
+    click.addEventListener('click', color);
+    let holidays = document.getElementsByClassName('holiday');
+
+    function color() {
+        for (let i = 0; i < holidays.length; i += 1) {
+            if (holidays[i].style.backgroundColor == 'pink') {
+                holidays[i].style.backgroundColor = 'rgb(238,238,238)';
+            } else {
+                holidays[i].style.backgroundColor = 'pink';
+            }
+        }
+    }
+}
+
+changeColorHoliday();
+
+function createButtonFriday(sexta) {
+    let container = document.getElementsByClassName('buttons-container')[0];
+    let newButton = document.createElement('button');
+    newButton.innerText = sexta;
+    newButton.id = 'btn-friday';
+
+    container.appendChild(newButton);
+}
+
+createButtonFriday('Sexta-feira');
+
+function buttonSextou() {
+    let click = document.getElementById('btn-friday');
+    click.addEventListener('click', sextou);
+
+    function sextou() {
+        let sextas = document.getElementsByClassName('friday');
+        let fridays = [4, 11, 18, 25];
+
+        for (let i = 0; i < sextas.length; i += 1) {
+            if (sextas[i].innerHTML !== 'SEXTOU') {
+                sextas[i].innerHTML = 'SEXTOU';
+            } else {
+                sextas[i].innerHTML = fridays[i];
+            }
+        }
+
+    }
+}
+
+buttonSextou();
+
+function addAndRemoveZoom() {
+    const zoom = document.getElementById('days')
+    zoom.addEventListener('mouseover', add);
+    zoom.addEventListener('mouseout', remove);
+
+    function add(event) {
+        event.target.style.fontSize = '30px';
+    }
+
+    function remove (event) {
+        event.target.style.fontSize = '20px'
+    }
+}
+
+addAndRemoveZoom();
+
+function createTasks (nomeDaTarefa) {
+    let container = document.getElementsByClassName('my-tasks')[0];
+    let newSpan = document.createElement('span');
+    newSpan.innerText = nomeDaTarefa;
+    
+    container.appendChild(newSpan);
+
+}
+
+createTasks ('estudar');
+
+function addBackColor (cor) {
+    let container = document.getElementsByClassName('my-tasks')[0];
+    let newDiv = document.createElement('div');
+    newDiv.className = 'task';
+    newDiv.style.backgroundColor = cor;
+
+    container.appendChild(newDiv);
+
+}
+
+addBackColor('yellow');
+
+function addEventNaBola () {
+    let botaoTask = document.querySelector('.task');
+    botaoTask.addEventListener('click', mudaClass);
+
+    function mudaClass () {
+        if (botaoTask.className == 'task') {
+            botaoTask.className = 'task selected';
+        } else {
+            botaoTask.className = 'task'
+        }
+    }
+
+}
+
+addEventNaBola();
+
+function addEventInDay () {
+    let botaoTask = document.getElementById('days');
+    botaoTask.addEventListener('click', mudaColor);
+
+    function mudaColor (event) {
+        if (event.target.style.color == 'yellow') {
+            event.target.style.color = 'rgb(119,119,119)';
+        } else {
+            event.target.style.color = 'yellow';
+        }
+    }
+
+}
+
+addEventInDay();
+
+function addCompromisso () {
+    let input = document.getElementById('task-input');
+    let botao = document.getElementById('btn-add');
+
+    input.addEventListener('keyup', function (event) {
+        if (event.key == 'Enter') {
+            newEvent();
+        }
+    });
+    botao.addEventListener('click', newEvent);
+
+    function newEvent () {
+        let container = document.getElementsByClassName('task-list')[0];
+        let newLi = document.createElement('li');
+
+        if (input.value.length > 0) {
+            newLi.innerText = input.value;
+            container.appendChild(newLi);
+        } else {
+            alert ('Você precisa inserir algo!')
+        }
+    }
+
+}
+
+addCompromisso();
