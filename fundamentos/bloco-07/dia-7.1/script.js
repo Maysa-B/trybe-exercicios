00ae7fe4b245a4a93428f914052cfa420975f416
Maysa <maybsalvalaio@gmail.com>
2022-04-01 19:09:56 -0300

    Exercícios parte 2

--- fundamentos/bloco-07/dia-7.1/script.js
+++ fundamentos/bloco-07/dia-7.1/script.js
@@ -1,4 +1,5 @@
 //parte 1
+//atividade 1
 let testingScope = (escopo) => {
     if (escopo === true) {
         let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -13,6 +14,7 @@ let testingScope = (escopo) => {
 
 testingScope(true);
 
+//atividade 2
 let funcaoCrescente = () => {
     const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
@@ -30,6 +32,8 @@ let funcaoCrescente = () => {
 
 console.log(funcaoCrescente());
 
+//parte 2
+//atividade 1
 let fatorial = number => {
     let result = number;
 
@@ -46,6 +50,7 @@ let fatorial = number => {
 
 console.log(fatorial(0));
 
+//atividade 2
 let biggestWord = (phrase) => {
     let palavraAtual = '';
     let maior = '';
@@ -61,7 +66,7 @@ let biggestWord = (phrase) => {
             }
         } else if (id === phrase.length - 1) {
             palavraAtual += phrase[id];
-            
+
             if (palavraAtual.length > maior.length) {
                 maior = palavraAtual;
                 palavraAtual = '';
@@ -77,3 +82,39 @@ let biggestWord = (phrase) => {
 }
 
 console.log(biggestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
+
+//atividade 3
+const botao = document.getElementById('botao');
+const paragrafo = document.getElementById('paragrafo');
+
+let clickCount = 0;
+
+botao.addEventListener('click', () => {
+    clickCount += 1;
+
+    paragrafo.innerText = `você clicou ${clickCount} vezes `;
+
+})
+
+//atividade 4
+let substituirX = (word) => `A vida é um(a) ${word}!`;
+
+let minhaSkill = ['javaScript', 'CSS', 'HTML', 'Git', 'BootsTrap'];
+
+let funcaoDois = (retorno) => {
+    minhaSkill.sort();
+
+    const result = document.getElementById('result');
+
+    let final = `${retorno} Minhas cinco principais habilidades são:
+    ${minhaSkill[0]}
+    ${minhaSkill[1]}
+    ${minhaSkill[2]}
+    ${minhaSkill[3]}
+    ${minhaSkill[4]}
+    #goTrybe`
+
+    result.innerText = final;
+}
+
+funcaoDois((substituirX('boneca')));
