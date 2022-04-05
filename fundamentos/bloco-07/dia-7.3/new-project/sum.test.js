9807656a2396311d29c81218e6fc0aad41396379
Maysa <maybsalvalaio@gmail.com>
2022-04-05 19:38:11 -0300

    Exercício 1

--- fundamentos/bloco-07/dia-7.3/new-project/sum.test.js
+++ fundamentos/bloco-07/dia-7.3/new-project/sum.test.js
@@ -1,5 +1,20 @@
-const sum = (a, b) => a + b;
+const sum = require('./sum');
 
-test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
-});
+describe('Função sum', () => {
+
+  test('4 mais 5 é 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  test('0 mais 0 é 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  test('Exibe erro', () => {
+    expect(() => {sum(4, '5')}).toThrow();
+  })
+
+  test('Exibe erro: parameters must be numbers', () => {
+    expect(() => {sum(4, '5')}).toThrowError(new Error('parameters must be numbers'));
+  })
+}) 
