0d467881a442292377e9a40127a3070b252578cf
Maysa <maybsalvalaio@gmail.com>
2022-04-05 20:22:40 -0300

    Exercício 3

--- fundamentos/bloco-07/dia-7.3/new-project/sum.test.js
+++ fundamentos/bloco-07/dia-7.3/new-project/sum.test.js
@@ -1,4 +1,4 @@
-const {sum, myRemove} = require('./sum');
+const {sum, myRemove, myFizzBuzz} = require('./sum');
 
 describe('Função sum', () => {
   test('4 mais 5 é 9', () => {
@@ -29,5 +29,33 @@ describe('Função myRemove', () => {
 
   test('Se mandar tirar um número que não existe, continua tudo igual', () => {
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  })
+})
+
+// Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
+// Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
+
+describe('Função myFizzBuzz', () => {
+  test('15 retorna FizzBuzz', () => {
+    expect(myFizzBuzz(15)).toMatch('fizzbuzz');
+  })
+  
+  test('9 retorna Fizz', () => {
+    expect(myFizzBuzz(9)).toMatch('fizz');
+  })
+
+  test('10 retorna Buzz', () => {
+    expect(myFizzBuzz(10)).toMatch('buzz');
+  })
+
+  test('13 retorna 13', () => {
+    expect(myFizzBuzz(13)).toBe(13);
+  })
+
+  test('inserir uma string retorna falso', () => {
+    expect(myFizzBuzz('15')).toBeFalsy();
   })
 })
